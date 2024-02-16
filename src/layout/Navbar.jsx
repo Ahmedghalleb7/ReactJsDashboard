d7e60d8f14dcdcc940a38e93a95f151b1d5e6f41
ahmed <ghallebahmed070215@gmail.com>
2024-02-16 09:51:15 +0100

    last version

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,21 @@
 import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBBtn,
+  MDBInputGroup
+} from 'mdb-react-ui-kit';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search'
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from "@mui/material/colors";
+
 
 function Navbar({ setToggle, toggle }) {
 
@@ -8,14 +25,70 @@ function Navbar({ setToggle, toggle }) {
       className={`position-fixed bg-dark ${
         toggle === true ? "justify-content-between" : "justify-content-end"
       }  d-flex`}
-      style={{ width: "100%", height: 70 }}
+      style={{ width: "100%", height: 70 , zIndex:2 }}
     >
       {toggle === true && (
         <button className="btn btn-light" onClick={() => setToggle(false)}>{"->"}</button>
       )}
-      <p className="text-white">navbar</p>
+      <Search className="p-2 m-2 d-flex aligne-items-center">
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
     </div>
+  
   );
+  
+
 }
 
+const Search = styled('div')(({ theme }) => ({
+  display:"flex",
+  alignItems:"center",
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "white",
+  '&:hover': {
+    backgroundColor: "white",
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
 export default Navbar;
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Try } from '@mui/icons-material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Dashboard() {
+  const [data,setData]= useState([])
+
+
+  useEffect(()=>{
+
+(
+  async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/data.json")
+      setData(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)()
+
+  },[])
   return (
-    <div style={{ paddingTop : 70 ,paddingLeft:300 }}>
-      dashboard
+    <div style={{ paddingTop : 80 ,paddingLeft:300 }} className='d-flex flex-wrap gap-3 justify-content-center mb-5 pb-5'>
+      {data.map((e,i)=>
+      <Card style={{ width: '18rem' }} >
+      <Card.Img variant="top" src={e.imageURL} style={{objectfit:"contain"}} width={100} height={200}/>
+      <Card.Body className='d-flex flex-column justify-content-between'  >
+        <Card.Title>{e.productName}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">More</Button>
+      </Card.Body>
+    </Card>
+      
+      )}
     </div>
   )
 }
